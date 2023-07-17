9e47ec9fa3e0c162654550412ddd1533a45c3729
PietroMarilei~ <pietromrl0@gmail.com>
2023-07-17 17:30:09 +0200

    card working

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -12,16 +12,24 @@ export default {
 </script>
 
 <template>
-    <!-- header template -->
+   
    <header>
-    HEADER
+    <img src="https://vignette1.wikia.nocookie.net/yugiohenespanol/images/a/aa/Yugioh_anime_logo.jpg/revision/latest?cb=20130310042737&path-prefix=es" alt="">
+    <h1>Yi-gi-oh API</h1>
   </header>
 
 </template>
 
 <style lang="scss" scoped>
     header {
+        display: flex;
+        
+        background-color: white;
         padding: 1rem;
-        text-align: center;
+
+        img {
+          width: 100px;
+        }
+        
     }
 </style>
--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,20 +1,44 @@
 <script>
+import { store } from '../store';
+
+
 export default {
   data() {
     return {
-      greeting: 'Hello World!'
+      
+      store,
     }
   },
   methods: {
 
   },
+  props: {
+
+  },
+
 }
 </script>
 
 <template>
     <div class="container">
       <div class="row">
-        <h1 class="text-center color-primary">Bootstrap is here !</h1>
+        <h1 class=" color-primary">dropdown</h1>
+        <div class="row">
+          <div class="col-sm-12 col-md-4 column" v-for="(singleCard,i) in store.cardArr" :key="i" >
+            <div class="my_card h-100">
+              <img :src="singleCard.card_images[0].image_url" alt="" class="img-fluid ">
+              <h3> {{singleCard.name }}</h3>
+              <div class="cardType">
+                <p>{{ singleCard.type }}</p>
+              </div>
+                <!-- {{store.cardArr[2].card_images[0].image_url }} -->
+            </div>
+            
+            
+            
+          </div>
+          
+        </div>
       </div>
     </div>
   
@@ -22,5 +46,11 @@ export default {
 </template>
 
 <style lang="scss" scoped>
+@use "../assets/scss/partials/variables.scss" as *;
+
+.my_card {
+  background-color: $card_yellow;
+  padding: 0.5rem;
+}
     
 </style>
